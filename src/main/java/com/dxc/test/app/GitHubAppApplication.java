22bc706638ab3b7abaa851228777bb5ce54a36a3
Shivaprasad Hegde <shivaprasad.hegde@hpe.com>
2017-09-28 15:12:31 +0530

    Comment Added

--- src/main/java/com/dxc/test/app/GitHubAppApplication.java
+++ src/main/java/com/dxc/test/app/GitHubAppApplication.java
@@ -2,11 +2,17 @@ package com.dxc.test.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*/
+ * 
+ * GitHubAppApplication
+ * 
+ */
 @SpringBootApplication
 public class GitHubAppApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(GitHubAppApplication.class, args);
+		
 	}
 }
