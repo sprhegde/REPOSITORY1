00bf00a59f2d646d3bc1c163b19a0ea301687fc4
Shivaprasad Hegde <shivaprasad.hegde@hpe.com>
2017-09-28 14:22:44 +0530

    First commit

---
+++ src/main/java/com/dxc/test/app/GitHubAppApplication.java
@@ -0,0 +1,12 @@
+package com.dxc.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dxc/test/app/controller/HelloRestController.java
@@ -0,0 +1,20 @@
+package com.dxc.test.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/*
+ * This project is created with 'New Maven Project'
+ * try http://localhost:9393/hello in the browser.
+ */
+@RestController
+public class HelloRestController {
+
+	@RequestMapping("/hello")
+	public String sayHi(@RequestParam(value="name", defaultValue="Rama") String name) {
+		
+		return "Hello "+name;
+		
+	}
+
+}
