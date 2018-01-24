5879efad16b7845a8a3f449912cfccac62a01336
Shivaprasad Hegde <shivaprasad.hegde@hpe.com>
2018-01-24 10:29:54 +0530

    SendGrid API key details removed

--- src/main/java/com/dxc/test/app/controller/HelloRestController.java
+++ src/main/java/com/dxc/test/app/controller/HelloRestController.java
@@ -17,25 +17,7 @@ public class HelloRestController {
 	public String sayHi(@RequestParam(value="name", defaultValue="Rama") String name) {
 		
 		
-		String sendEmailResult="";
-		
-		SendGrid sendgrid = new SendGrid("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");
-		SendGrid.Email welcomeMail = new SendGrid.Email();
-		welcomeMail.addTo("shivaprasad.hegde@hpe.com");
-		welcomeMail.addToName("User-san");
-		welcomeMail.setFrom("welcome@example.com");
-		welcomeMail.setSubject("Welcome to Example!");
-		welcomeMail.setText("Thank you for your interest in Example.com! It is still in Beta at the moment but there are a number of exciting features planned. Tell us what you'd like to see.");
-
-		try {
-		    SendGrid.Response response = sendgrid.send(welcomeMail);
-		    sendEmailResult=response.getMessage();
-		} catch (SendGridException sge) {
-			 sendEmailResult="ERROR IS"+sge.getMessage();
-		}
-		
-		
-		return "This is my first sample REST service application with Spring Boot Technology- "+name+" Email Result is "+sendEmailResult;
+		return "This is my first sample REST service application with Spring Boot Technology- "+name;
 		
 	}
 
